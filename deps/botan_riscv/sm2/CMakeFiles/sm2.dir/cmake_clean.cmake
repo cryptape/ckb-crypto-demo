1975f996cb9c1d6ddaa7a33fc3fe6f75d66886dd
GaoGGGQY <GQQQy@buaa.edu.cn>
2020-05-11 16:45:27 +0800

    sm2

---
+++ deps/botan_riscv/sm2/CMakeFiles/sm2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sm2.dir/sm2.cpp.obj"
+  "sm2.pdb"
+  "sm2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sm2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
