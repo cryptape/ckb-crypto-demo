6c6f43cf79a5a9c18aad404ad3160ad3f5d3a5b0
Mona Lisa <929397876@qq.com>
2020-05-15 10:29:52 +0800

    优化SM2

--- src/sm2/verify.cpp
+++ src/sm2/verify.cpp
@@ -8,14 +8,17 @@
 #include <botan/pkcs8.h>
 #include <botan/x509_key.h>
 #include <botan/point_gfp.h>
+#include "ckb_syscalls.h"
 
 int main()
 {
 
     std::string text("This is a tasty burger!");
-    std::vector<uint8_t> data(text.data(),text.data()+text.length());
+   std::vector<uint8_t> data(text.data(),text.data()+text.length());
 
-    Botan::EC_Group ecgrp("sm2p256v1");
+
+    //Botan::EC_Group ecgrp("sm2p256v1");
+    Botan::EC_Group ecgrp(Botan::OID({1,2,156,10197,1,301}));
 
     std::string pubkey_s = "039DBCCE0D6A94BC702037D9C593391ED2B2168E5482DE7593CAE3E3BE86317189";
     std::string signature_s = "29F00228BFBF38178AEDFD67075D0CAAC977EA2C310817D8A4F52E9C4ACF57AE02B3230BB120364A92ED1ACE065FCB0729532DE5DDA77A09CA48167A5EADE171";
@@ -29,10 +32,11 @@ int main()
     pkVerifier.update(data);
 
     int result = pkVerifier.check_signature(Botan::hex_decode(signature_s));
-    //std::cout << "is " << (re? "valid\n" : "invalid\n") << std::endl;
+//std::cout << "is " << (re? "valid\n" : "invalid\n") << std::endl;
 
     if(result){
-        return 0;
+        ckb_exit (0);
     } else
-        return 1;
+        ckb_exit (1);
+
 }
